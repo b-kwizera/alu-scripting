007362e839d75b2a46a747feec2f0792935e6408
Bodgar <b.kwizera@alustudent.com>
2025-09-05 07:39:15 -0700

    Add regex script to match hbtn with zero or more b's

--- regular_expressions/4-repetition_token_3.rb
+++ regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,27 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb*tn/).join
+
+# Check if an argument is provided
+if ARGV.length != 1
+  puts "Usage: #{$0} <string_to_match>"
+  exit 1
+end
+
+# Get the input string from command line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+# Note: No square brackets allowed in this regex
+# TODO: Replace with actual pattern based on test cases
+pattern = /PATTERN_HERE/
+
+# Find all matches
+matches = input_string.scan(pattern)
+
+# Output the matches
+if matches.empty?
+  puts ""
+else
+  matches.each do |match|
+    puts match
+  end
+end
