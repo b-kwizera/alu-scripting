02892fe9da25a46bac74a00cf272bae753e4e94d
Bodgar <b.kwizera@alustudent.com>
2025-09-05 07:25:41 -0700

    Add repetition token regular expression script

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,2 +1,26 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb{1,2}tn/).join
+
+# Check if an argument is provided
+if ARGV.length != 1
+  puts "Usage: #{$0} <string_to_match>"
+  exit 1
+end
+
+# Get the input string from command line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+# TODO: Replace this with the actual pattern based on your requirements
+pattern = /YOUR_REGEX_PATTERN_HERE/
+
+# Find all matches
+matches = input_string.scan(pattern)
+
+# Output the matches
+if matches.empty?
+  puts "No matches found"
+else
+  matches.each do |match|
+    puts match
+  end
+end
