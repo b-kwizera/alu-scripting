e66fa875b569ef907c42d36b0c2480d36644152f
bogdax <bogdax@BogdarK-HP.>
2025-10-26 01:18:33 +0200

    Implemented top_ten to fetch first 10 hot posts from a subreddit

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and prints the titles of the first 10 hot posts
+for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+        """Prints the titles of the first 10 hot posts for a subreddit.
+
+            Args:
+                        subreddit (str): The subreddit to query.
+                            """
+                                url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+                                    headers = {"User-Agent": "alu-scripting/1.0"}
+                                        params = {"limit": 10}
+
+                                            try:
+                                                    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+                                                            if response.status_code != 200:
+                                                                        print(None)
+                                                                                    return
+
+                                                                                            data = response.json().get("data", {}).get("children", [])
+                                                                                                    for post in data:
+                                                                                                                title = post.get("data", {}).get("title")
+                                                                                                                            if title:
+                                                                                                                                            print(title)
+                                                                                                                                                except Exception:
+                                                                                                                                                        print(None)
+
